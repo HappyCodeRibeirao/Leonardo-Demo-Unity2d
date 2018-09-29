ff707daebad47d9286b88beb4a41ca85977e1ab6
happycode <happycode.ribeiraopreto@gmail.com>
2018-09-29 12:24:51 -0300

    continuação da aula do unity

---
+++ LeonardoDemo/Assets/coinController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinController : MonoBehaviour {
+	int vidas = 4;
+	string nomeHeroi = "Jon";
+	// Use this for initialization
+	void Start () {
+		Debug.Log (falarNome ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public string falarNome()
+	{
+		return nomeHeroi;
+
+	}
+
+}
